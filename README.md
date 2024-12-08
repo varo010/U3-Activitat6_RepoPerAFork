5faa415f3184dd9a0cfb5302912bb865a49382ea
Empar <e.carrasquermoya@edu.gva.es>
2024-12-08 09:56:19 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,129 @@
+# Activitat6_RepoPerAFork
+
+## Repositori per a Fork.
+### -------------------- INSTRUCCIONS --------------------------
+###### ATENCIÓ: NO HAS DE MODIFICAR ESTES INSTRUCCIONS
+### Una vegada has fet el fork d'aquest repositori:
+
+*Des del Terminal:*
+
+* Clona't en local la teua còpia del repositori del qual has fet el fork
+* Crea't una rama, nomenada amb el teu nom:  "teu_nom"
+* Canvia a la nova rama. 
+* Has de editar el fitxer .java *modificant*, el mètode que et corresponga. AlumneFilaxxxPCxxx() {}. 
+El mètode imprimirà el teu nom i una felicitació i desig per a l'Any Nou.
+* El mètode main() no l'has de modificar !
+* Comprova que el programa executa bé.
+* Confirma els canvis fets en el repositori local.
+* Puja els canvis al repositori remot.
+
+*Des de GitHub:*
+
+* Modifica el README.md on et corresponga, on descrius la tasca que has fet.
+* Fes un *pull-request* per a demanar al propietari del repositori (que és el professor) que revise la teua feina i que incorpore a la rama principal els canvis de la teua rama.
+
+
+
+### ------------------- FI INSTRUCCIONS -------------------------------------------
+
+
+#### PROFESSOR:   EMPAR
+Empar ha afegit el mètode que desitja: "Feliç Any Nou.  Pau i Salut per a tots."
+
+
+## A PARTIR D'ACÍ HAS DE DESCRIURE, ON ET CORRESPON, ELS CANVIS QUE HAS FET:
+## --> POSA EL TEU NOM I EN LA SEGÜENT LÍNIA LA DESCRIPCIÓ DE LA TASCA
+
+
+#### AlumneFila1 PC1: 
+
+
+#### AlumneFila1 PC2:
+
+
+#### AlumneFila1 PC3:
+
+
+#### AlumneFila1 PC4:
+
+
+#### AlumneFila1 PC5:
+
+
+#### AlumneFila1 PC6:
+
+
+#### AlumneFila2 PC1:
+
+
+#### AlumneFila2 PC2:
+
+
+#### AlumneFila2 PC3:
+
+
+#### AlumneFila2 PC4:
+
+
+#### AlumneFila2 PC5:
+
+
+#### AlumneFila2 PC6:
+
+
+
+#### AlumneFila3 PC1:
+
+
+#### AlumneFila3 PC2:
+
+
+#### AlumneFila3 PC3:
+
+
+#### AlumneFila3 PC4:
+
+
+#### AlumneFila3 PC5:
+
+
+#### AlumneFila3 PC6:
+
+
+
+#### AlumneFila4 PC1:
+
+
+#### AlumneFila4 PC2:
+
+
+#### AlumneFila4 PC3:
+
+
+#### AlumneFila4 PC4:
+
+
+#### AlumneFila4 PC5:
+
+
+#### AlumneFila4 PC6:
+
+
+
+#### AlumneFila5 PC1:
+
+
+#### AlumneFila5 PC2:
+
+
+#### AlumneFila5 PC3:
+
+
+#### AlumneFila5 PC4:
+
+
+#### AlumneFila5 PC5:
+
+
+#### AlumneFila5 PC6:
+
---
+++ src/ed/empar/MissatgesNadalencs.java
@@ -0,0 +1,229 @@
+package ed.empar;
+
+/**
+ * Missatge i desitjos per a l'Any Nou
+ *
+ * @author EMPAR
+ * @version 1.0
+ *
+ */
+
+public class MissatgesNadalencs {
+	////
+	// ATENCIÓ: el main() no l'has de modificar !!!!!!!!!!!
+	///
+	public static void main(String[] args) {
+		
+		missatgeProfessor();
+
+		// Fila1
+		AlumneFila1PC1();
+
+		AlumneFila1PC2();
+
+		AlumneFila1PC3();
+
+		AlumneFila1PC4();
+
+		AlumneFila1PC5();
+
+		AlumneFila1PC6();
+
+		// Fila2
+		AlumneFila2PC1();
+
+		AlumneFila2PC2();
+
+		AlumneFila2PC3();
+
+		AlumneFila2PC4();
+
+		AlumneFila2PC5();
+
+		AlumneFila2PC6();
+
+		// Fila3
+		AlumneFila3PC1();
+
+		AlumneFila3PC2();
+
+		AlumneFila3PC3();
+
+		AlumneFila3PC4();
+
+		AlumneFila3PC5();
+
+		AlumneFila3PC6();
+		
+
+		// Fila4
+		AlumneFila4PC1();
+
+		AlumneFila4PC2();
+
+		AlumneFila4PC3();
+
+		AlumneFila4PC4();
+
+		AlumneFila4PC5();
+
+		AlumneFila4PC6();
+		
+		// Fila5
+		AlumneFila5PC1();
+
+		AlumneFila5PC2();
+
+		AlumneFila5PC3();
+
+		AlumneFila5PC4();
+
+		AlumneFila5PC5();
+
+		AlumneFila5PC6();
+
+	}
+
+	// NO modifiques este métode
+	private static void missatgeProfessor() {
+		System.out.println("====================================");
+		System.out.println("Hola, sóc EMPAR la professora de ED");
+		System.out.println(" FELIÇ ANY NOU !!!!!!");
+		System.out.println(" Salut i Pau per a tots.");
+		System.out.println("====================================");
+		System.out.println();
+	}
+
+	
+	// ********************************************************
+	// PROGRAMA el mètode que et correspon amb el teu misssatge
+	// Impremeix una línia en blanc després del teu missatge
+	// ********************************************************
+
+	// FILA 1
+	private static void AlumneFila1PC1() {
+
+	}
+
+	private static void AlumneFila1PC2() {
+
+	}
+
+	private static void AlumneFila1PC3() {
+
+	}
+
+	private static void AlumneFila1PC4() {
+
+	}
+
+	private static void AlumneFila1PC5() {
+
+	}
+
+	private static void AlumneFila1PC6() {
+
+	}
+
+	// FILA 2
+	private static void AlumneFila2PC1() {
+
+	}
+
+	private static void AlumneFila2PC2() {
+
+	}
+
+	private static void AlumneFila2PC3() {
+
+	}
+
+	private static void AlumneFila2PC4() {
+
+	}
+
+	private static void AlumneFila2PC5() {
+
+	}
+
+	private static void AlumneFila2PC6() {
+
+	}
+
+	// FILA 3
+	private static void AlumneFila3PC1() {
+
+	}
+
+	private static void AlumneFila3PC2() {
+
+	}
+
+	private static void AlumneFila3PC3() {
+
+	}
+
+	private static void AlumneFila3PC4() {
+
+	}
+
+	private static void AlumneFila3PC5() {
+
+	}
+
+	private static void AlumneFila3PC6() {
+
+	}
+
+	// FILA 4
+	private static void AlumneFila4PC1() {
+
+	}
+
+	private static void AlumneFila4PC2() {
+
+	}
+
+	private static void AlumneFila4PC3() {
+
+	}
+
+	private static void AlumneFila4PC4() {
+
+	}
+
+	private static void AlumneFila4PC5() {
+
+	}
+
+	private static void AlumneFila4PC6() {
+		
+	}
+	
+	// FILA 5
+	private static void AlumneFila5PC1() {
+
+	}
+
+	private static void AlumneFila5PC2() {
+
+	}
+
+	private static void AlumneFila5PC3() {
+
+	}
+
+	private static void AlumneFila5PC4() {
+
+	}
+
+	private static void AlumneFila5PC5() {
+
+	}
+
+	private static void AlumneFila5PC6() {
+		
+	}
+
+	
+}
